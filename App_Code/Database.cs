cf1f7e23e9251528f937171c3f9055fd99dcaf84
Kye Jordan O'Reilly <kye_jrdn@hotmail.com>
2016-11-23 18:38:31 +0800

    Addming Admin Website Template

---
+++ App_Code/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Database
+/// </summary>
+public class Database
+{
+    string connectionString = "Server='wmaproject.database.windows.net';Database=hlgroup;" +
+        "User ID=wmaproject;Password=Password123;";
+    public Database()
+    {
+       
+    }
+
+    public SqlConnection getDBConnection()
+    {
+        SqlConnection connection = new SqlConnection(connectionString);
+        return connection;
+    }
+}
